6147850fb2e258a6628abcae9846ce3846e32768
M. Zuhaib <41955683+zuhaibranaa@users.noreply.github.com>
2023-08-11 07:52:17 +0000

    installed vuetify

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,6 +3,16 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 import io from "socket.io-client";
+// Vuetify
+import "vuetify/styles";
+import { createVuetify } from "vuetify";
+import * as components from "vuetify/components";
+import * as directives from "vuetify/directives";
+
+const vuetify = createVuetify({
+  components,
+  directives,
+});
 
 const socket = io("http://localhost:3000");
 const SocketPlugin = {
@@ -15,4 +25,9 @@ const savedState = JSON.parse(localStorage.getItem("vuexState"));
 if (savedState) {
   store.replaceState(savedState);
 }
-createApp(App).use(SocketPlugin).use(store).use(router).mount("#app");
+createApp(App)
+  .use(SocketPlugin)
+  .use(vuetify)
+  .use(store)
+  .use(router)
+  .mount("#app");
